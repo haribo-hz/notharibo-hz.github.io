693b88fff84d193a907b27b46d211eaed28f2770
willow <42willow@pm.me>
2025-01-25 21:24:09 +1100

    chore: update `baseurl`

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "haribo-hz.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
